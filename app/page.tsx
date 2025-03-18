5ab18f59dcd6ab8944f5844a082eaa1cb10abbf1
Whydandrian <whydandrrian@gmail.com>
2025-03-18 15:47:13 +0800

    add changes into login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,28 @@
 "use client";
 import {Button} from "@/components/ui/button";
+import { Poppins } from "next/font/google";
 
-export default function Home() {
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+})
 
-    console.log("where iam")
-    
+export default function Home() {    
 
     return (
-        <Button size="lg" variant="destructive">
-            Click Me
-        </Button>
+        <main className="flex h-full flex-col
+            items-center justify-center
+            bg-gradient-to-r from-slate-50 to-sky-100">
+          
+          <div className="space-y-6">
+            <h1 className="text-6xl font-semibold text-white drop-shadow-md">
+              Auth whydandrian
+            </h1>
+            <p className="text-center text-lg text-white">
+              Simple Authentication using NextJS
+            </p>
+          </div>
+
+        </main>
     )
 }
